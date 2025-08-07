1b89868926bf1cbf038735a0fbdf7c50afc13211
Workshop Participant <participants@workshops.aws>
2025-08-07 12:01:41 -0400

    Adding Solution Tracking thru Open Search

--- platform/infra/terraform/mgmt/terraform/mgmt-cluster/variables.tf
+++ platform/infra/terraform/mgmt/terraform/mgmt-cluster/variables.tf
@@ -6,5 +6,5 @@
 variable "usage_tracking_tag" {
   description = "Tag to identify Modern Engineering on AWS usage within tracking tools"
   type        = string
-  default     = "(uksb-u7h6yeonth)"
+  default     = "uksb-u7h6yeonth"
 }
