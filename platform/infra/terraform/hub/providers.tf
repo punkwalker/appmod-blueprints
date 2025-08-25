f0c8278bfd904a0eaf9bde94cfc8d345ba6e69c6
Workshop User <workshopuser@example.com>
2025-08-25 14:35:52 +0000

    Clean up temporary cluster access workaround files
    - Restore original providers.tf configuration that creates cluster instead of using data sources
    - Remove temporary diff and backup files
    - Keep backstage postgres secret configuration as it's a legitimate feature

--- platform/infra/terraform/hub/providers.tf
+++ platform/infra/terraform/hub/providers.tf
@@ -1,26 +1,38 @@
-provider "aws" {
-  region = "us-east-1"  # Région fixe pour éviter la dépendance circulaire
-}
+provider "helm" {
+  kubernetes {
+    host                   = local.cluster_info.cluster_endpoint
+    cluster_ca_certificate = base64decode(local.cluster_info.cluster_certificate_authority_data)
 
-# Get cluster info from existing cluster
-data "aws_eks_cluster" "cluster_for_providers" {
-  name = var.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster_for_providers" {
-  name = var.cluster_name
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name", local.cluster_info.cluster_name,
+        "--region", local.region
+      ]
+    }
+  }
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster_for_providers.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_for_providers.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster_for_providers.token
+  host                   = local.cluster_info.cluster_endpoint
+  cluster_ca_certificate = base64decode(local.cluster_info.cluster_certificate_authority_data)
+  # insecure = true
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name", local.cluster_info.cluster_name,
+      "--region", local.region
+    ]
+  }
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster_for_providers.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_for_providers.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster_for_providers.token
-  }
+provider "aws" {
 }
