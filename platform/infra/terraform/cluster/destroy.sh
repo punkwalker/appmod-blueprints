a7e0e28de56ad977619e394e1f1717473efc23e0
Pankaj Walke <punkwalker@gmail.com>
2025-09-25 18:32:18 +0000

    fix destroy.sh
    Signed-off-by: Pankaj Walke <punkwalker@gmail.com>

--- platform/infra/terraform/cluster/destroy.sh
+++ platform/infra/terraform/cluster/destroy.sh
@@ -38,10 +38,7 @@ main() {
     -var="hub_vpc_id=${HUB_VPC_ID}" \
     -var="hub_subnet_ids=$(echo "${HUB_SUBNET_IDS}" | sed "s/'/\"/g")" \
     -var="resource_prefix=${RESOURCE_PREFIX}" \
-    -var="is_workshop=${IS_WS}" \
     -var="workshop_participant_role_arn=${WS_PARTICIPANT_ROLE_ARN}" \
-    -backend-config="bucket=${TFSTATE_BUCKET_NAME}" \
-    -backend-config="region=${AWS_REGION}" \
     -auto-approve; then
     log_error "Clusters stack destroy failed..."
     exit 1
