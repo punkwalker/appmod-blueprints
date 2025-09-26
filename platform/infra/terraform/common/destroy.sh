ced206d48d1ef578852514475c7bd249c3ab7186
Pankaj Walke <punkwalker@gmail.com>
2025-09-26 01:29:33 +0000

    fix destroy.sh of boostrap
    Signed-off-by: Pankaj Walke <punkwalker@gmail.com>

--- platform/infra/terraform/common/destroy.sh
+++ platform/infra/terraform/common/destroy.sh
@@ -38,6 +38,7 @@ main() {
   yq eval -o=json '.' $CONFIG_FILE > $GENERATED_TFVAR_FILE
 
   if ! $SKIP_GITLAB ; then
+    initialize_terraform "gitlab infra" "$DEPLOY_SCRIPTDIR/gitlab_infra"
     GITLAB_DOMAIN=$(terraform -chdir=$DEPLOY_SCRIPTDIR/gitlab_infra output -raw gitlab_domain_name)
     GITLAB_SG_ID=$(terraform -chdir=$DEPLOY_SCRIPTDIR/gitlab_infra output -raw gitlab_security_groups)
   fi
